fc52fbc9152d6c1d22dd9cbaffb42bf2555fc996
Alena Klyachina <kliachina.av@phystech.edu>
2021-10-13 03:42:28 +0300

    Игра "поймай шарик". готовая версия

--- Tkinter/поймай шарик.py
+++ Tkinter/поймай шарик.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from random import randrange as rnd, choice
-import time
+
 
 root = Tk()
 root.geometry('800x600')
@@ -8,66 +8,85 @@ root.geometry('800x600')
 canv = Canvas(root, bg='white')
 canv.pack(fill=BOTH, expand=1)
 
-l = Label(root, bg='white', fg='black', width=40)
+label = Label(root, bg='white', fg='black', width=40)
 global score
 score = 0
-l['text'] = 'score: ' + str(score)
-l.pack()
-colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink', 'lightblue', 'lightgreen', 'brown', 'black']
-balls = set()
-removedBalls = set()
+label['text'] = 'score: ' + str(score)
+label.pack()
+colors = ['salmon', 'orange', 'yellow', 'greenyellow', 'plum', 'pink', 'lightblue', 'lightgreen', 'black', 'black']
+balls = []  # хранит данные о всех когда-либо существовавших шариках
+aliveBalls = set()   # множество номеров всех живых шариков из массива Balls
 
 def new_ball():
-    """функция отрисовывает шарик
-    x, y - координаты центра шарика"""
+    """функция создает новый шарик
+    x, y - координаты центра шарика
+    r - radius
+    color - ball's color
+    dx, dy - проекции вектора перемещения"""
     global balls
-    #canv.delete(ALL)
     x = rnd(100, 700)
     y = rnd(100, 500)
     r = rnd(30, 50)
     color = choice(colors)
-    dx = rnd(-30, 30)
-    dy = rnd(-30, 30)
-    circle = canv.create_oval(x-r, y-r, x+r, y+r, fill=color, width=0)
-    balls.add((circle, x, y, r, color))
-    #canv.move(circle, 10, 20)
-    root.after(1000, new_ball)
+    dx = rnd(-5, 5)
+    dy = rnd(-5, 5)
+    balls.append([x, y, r, color, dx, dy])
+    aliveBalls.add(len(balls)-1)
 
 def clickOnBall(event):
+    '''
+    функция определяет кликнули ли мышей по одному из живущих шариков
+    если да: убирает его
+    иначе: ничего не делает
+    '''
     global score
-    for ball in balls:
-        x = ball[1]
-        y = ball[2]
-        r = ball[3]
-        color = ball[4]
+    for i in aliveBalls:
+        x = balls[i][0]
+        y = balls[i][1]
+        r = balls[i][2]
+        color = balls[i][3]
         if (x - event.x)**2 + (y - event.y)**2 <= r**2:
             if color == 'black':
                 score = 0
-                l['text'] = 'score: 0 !!!DO NOT CLICK ON BOMBS!!!'
+                label['text'] = 'score: 0 !!!DO NOT CLICK ON BOMBS!!!'
             else:
                 score += 1
-                l['text'] = 'score: ' + str(score)
-            removedBalls.add(ball)
-            canv.create_oval(x-r, y-r, x+r, y+r, fill='white', width=0)
-    balls.difference_update(removedBalls)
-    removedBalls.clear()
+                label['text'] = 'score: ' + str(score)
+            aliveBalls.remove(i)
+
+def movingFunc():
+    '''
+    функция отрисовывает все шарики
+    пересчитывает координаты каждого, в зависимости от его положения и вектора скорости
+    '''
+    global balls
+    canv.delete(ALL)
+    for i in aliveBalls:
+        dx = balls[i][4]
+        dy = balls[i][5]
+        balls[i][0] += dx
+        balls[i][1] += dy
+        x = balls[i][0]
+        y = balls[i][1]
+        r = balls[i][2]
+        if y - r <= 5 or y + r >= 575:
+            balls[i][5] = -dy
+        if x - r <= 5 or x + r >= 795:
+            balls[i][4] = -dx
+        color = balls[i][3]
+        canv.create_oval(x-r, y-r, x+r, y+r, fill=color, width=0)
+
+def main():
+    if rnd(0, 20) == 1: new_ball()
+    movingFunc()
+    root.after(50, main)
 
 
-
-for i in range(rnd(1, 2)):
-    new_ball()
+main()
 canv.bind('<Button-1>', clickOnBall)
-
-
-print('kek')
 root.mainloop()
 
 
-
-
-#new_ball()
-#canv.bind('<Button-1>', clickOnBall)
-#root.mainloop()
 
 
 
